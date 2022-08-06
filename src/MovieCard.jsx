5970dd5349b28f0dbfd22019af25a5cc0eefe113
Ivan Salas <ivanedusalas@gmail.com>
2022-08-06 11:56:55 -0300

    Aplication with grit of movies

---
+++ src/MovieCard.jsx
@@ -0,0 +1,11 @@
+import styles from "./MovieCard.module.css"
+
+export function MovieCard({ movie }) {
+    const imagenUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path
+    return (
+         <li className={styles.movieCard}   >
+            <img width={230} height={345} className={styles.movieImage} src={imagenUrl} alt={movie.title} />
+            <div>{movie.title}</div>
+        </li>
+    )
+}
